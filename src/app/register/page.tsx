f1503801fd7e3627700896e39df02219827df3d9
Rampop01 <popoolarahmat96@gmail.com>
2025-04-27 04:29:56 +0100

    build error fixed

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { Header } from '@/components/Sidebar';
+
 
 function RegisterPage() {
   const router = useRouter();
